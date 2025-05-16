25bde43a4e323648d9f2fd41d290eef2be1cb7b5
Monami Yoshioka <monami.yoshioka@gmail.com>
2025-05-16 22:22:29 +0100

    random

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { PlotlyModule } from 'angular-plotly.js';
-import * as PlotlyJS from 'plotly.js-dist-min';
+import * as PlotlyJS from 'plotly.js-basic-dist';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
